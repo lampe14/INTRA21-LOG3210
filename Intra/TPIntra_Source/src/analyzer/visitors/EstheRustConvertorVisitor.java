7244d2db545ef35e3083af7ea2a65fdb8b0d4bde
Yuhan <yuhan-li@hotmail.com>
2021-03-14 16:22:27 -0400

    code 100% fonctionnel

--- Intra/TPIntra_Source/src/analyzer/visitors/EstheRustConvertorVisitor.java
+++ Intra/TPIntra_Source/src/analyzer/visitors/EstheRustConvertorVisitor.java
@@ -75,14 +75,32 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
     }
 
     @Override
-    public Object visit(ASTPrimitiveExpr node, Object data) {
-        node.childrenAccept(this, data);
+    public Object visit(ASTStringValue node, Object data) {
+        m_writer.print(node.getValue());
         return null;
     }
 
     @Override
-    public Object visit(ASTStringValue node, Object data) {
+    public Object visit(ASTBoolValue node, Object data) {
+        m_writer.print(capitalize(node.getValue()));
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTIntValue node, Object data) {
         m_writer.print(node.getValue());
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTRealValue node, Object data) {
+        m_writer.print(node.getValue());
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTPrimitiveExpr node, Object data) {
+        node.childrenAccept(this, data);
         return null;
     }
 
@@ -92,44 +110,8 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
         return null;
     }
 
-
-    private void printWhile(SimpleNode node, Object data) {
-        m_writer.print("while ");
-        node.jjtGetChild(0).jjtAccept(this,data);
-        m_writer.println(":");
-        indentationLevel++;
-        if(node.jjtGetChild(1).jjtGetNumChildren() == 0)
-            m_writer.println(INDENT + "pass");
-        else {
-            node.jjtGetChild(1).jjtAccept(this,data);
-            indentationLevel--;
-        }
-    }
-
-    @Override
-    public Object visit(ASTWhileStmt node, Object data) {
-        if(indentationLevel >= 1) {
-            for(int i = 0; i < indentationLevel; i++) {
-                m_writer.print(INDENT);
-                printWhile(node, data);
-            }
-        } else
-            printWhile(node, data);
-
-        return null;
-    }
-
-    @Override
-    public Object visit(ASTCoeurStmt node, Object data) {
-        if(indentationLevel >= 1) {
-            for(int i = 0; i < indentationLevel; i++)
-                m_writer.println(INDENT + "print(\"Compilateurs est le meilleur cours au monde\")");
-        } else
-            m_writer.println("print(\"Compilateurs est le meilleur cours au monde\")");
-
-        return null;
-    }
-
+    /////////////////////////////////////////////////////////////////////////////
+    //4.1 Importations
     @Override
     public Object visit(ASTImportStmt node, Object data) {
         if (node.jjtGetNumChildren() >= 1) {
@@ -142,6 +124,8 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
         return null;
     }
 
+    /////////////////////////////////////////////////////////////////////////////
+    //4.2 Declarations
     private void printDeclare(SimpleNode node, Object data) {
         node.jjtGetChild(0).jjtAccept(this, data);
         m_writer.print(" = ");
@@ -152,16 +136,13 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
     @Override
     public Object visit(ASTDeclareStmt node, Object data) {
         if(indentationLevel >= 1) {
-            for(int i = 0; i < indentationLevel; i++) {
+            for(int i = 0; i < indentationLevel; i++)
                 m_writer.print(INDENT);
-                printDeclare(node, data);
-            }
+            printDeclare(node, data);
         } else
             printDeclare(node, data);
         return null;
     }
-
-
 
     @Override
     public Object visit(ASTAssignStmt node, Object data) {
@@ -169,7 +150,10 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
         return null;
     }
 
-    private String getOp(String opValue) {
+    /////////////////////////////////////////////////////////////////////////////
+    //4.3 Operations arithmetiques
+
+    private String getOp(String opValue) { //puisque Python utilise des operateurs differents
         switch (opValue) {
             case "&&":
                 opValue = "and";
@@ -186,12 +170,9 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
 
     @Override
     public Object visit(ASTCompExpr node, Object data) {
-
         node.jjtGetChild(0).jjtAccept(this, data);
-        String opValue = node.getValue();
-
         if(node.getValue() != null)
-            m_writer.print(" " + getOp(opValue) + " ");
+            m_writer.print(" " + getOp(node.getValue()) + " ");
         if(node.jjtGetNumChildren() > 1)
             node.jjtGetChild(1).jjtAccept(this, data);
         return null;
@@ -204,16 +185,13 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
             m_writer.print(" " + node.getValue() + " ");
         if(node.jjtGetNumChildren() > 1)
             node.jjtGetChild(1).jjtAccept(this, data);
-
-
         return null;
     }
 
     @Override
     public Object visit(ASTUnaExpr node, Object data) {
-        String opValue = node.getValue();
         if(node.getValue() != null)
-            m_writer.print(getOp(opValue));
+            m_writer.print(getOp(node.getValue()));
         node.jjtGetChild(0).jjtAccept(this, data);
         if(node.jjtGetNumChildren() > 1)
             node.jjtGetChild(1).jjtAccept(this, data);
@@ -223,17 +201,18 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
 
     @Override
     public Object visit(ASTParExpr node, Object data) {
-
         if(node.jjtGetChild(0) instanceof ASTExpr) {
             m_writer.print("(");
             node.jjtGetChild(0).jjtAccept(this, data);
             m_writer.print(")");
-        } else
+        } else //Les valeurs primitives (Int, Real, etc)
             node.jjtGetChild(0).jjtAccept(this, data);
 
         return null;
     }
 
+    /////////////////////////////////////////////////////////////////////////////
+    // 4.4 Fonctions
     @Override
     public Object visit(ASTFunctionStmt node, Object data) {
         node.jjtGetChild(0).jjtAccept(this, data);
@@ -243,7 +222,7 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
         else
             node.jjtGetChild(1).jjtAccept(this, data);
         if(node.jjtGetNumChildren() == 3) {
-            m_writer.print("\t" + "return ");
+            m_writer.print(INDENT + "return ");
             node.jjtGetChild(2).jjtAccept(this, data);
             m_writer.println("");
         }
@@ -270,61 +249,74 @@ public class EstheRustConvertorVisitor implements ParserVisitor {
 
     @Override
     public Object visit(ASTCallFunction node, Object data) {
+        if(node.jjtGetParent() instanceof ASTStmt) { //pas indenté pour une assignation (AssignStmt)
+            for(int i = 0; i < indentationLevel; i++)
+                m_writer.print(INDENT);
+        }
+
         node.jjtGetChild(0).jjtAccept(this, data); //Identifier (nom de fct)
         m_writer.print("(");
         if(node.jjtGetNumChildren() > 1)
-        for(int i = 1; i < node.jjtGetNumChildren(); i++) {
-            node.jjtGetChild(i).jjtAccept(this, data);
-            if(i < node.jjtGetNumChildren() - 1)
-                m_writer.print(", ");
-        }
+            for(int i = 1; i < node.jjtGetNumChildren(); i++) {
+                node.jjtGetChild(i).jjtAccept(this, data);
+                if(i < node.jjtGetNumChildren() - 1)
+                    m_writer.print(", ");
+            }
         m_writer.print(")");
-
+        if(node.jjtGetParent() instanceof ASTStmt)
+            m_writer.println("");
         return null;
     }
 
-    private void printIf(SimpleNode node, Object data) {
-        m_writer.print("if ");
-        node.jjtGetChild(0).jjtAccept(this,data);
+    /////////////////////////////////////////////////////////////////////////////
+    // 4.5 Condition If ET 4.6 Boucle While
+    private void printBlock(SimpleNode node, int child, Object data) {
         m_writer.println(":");
         indentationLevel++;
-        if(node.jjtGetChild(1).jjtGetNumChildren() == 0)
-            m_writer.println(INDENT + "pass");
+        if(node.jjtGetChild(child).jjtGetNumChildren() == 0) {
+            for(int i = 0; i < indentationLevel; i++)
+                m_writer.print(INDENT);
+            m_writer.println("pass");
+        }
         else
-            node.jjtGetChild(1).jjtAccept(this,data);
+            node.jjtGetChild(child).jjtAccept(this,data);
+        indentationLevel--;
     }
 
     @Override
     public Object visit(ASTIfStmt node, Object data) {
-        if(indentationLevel >= 1) {
-            for(int i = 0; i < indentationLevel; i++) {
-                m_writer.print(INDENT);
-                printIf(node, data);
-                indentationLevel--;
-            }
-        } else {
-            printIf(node, data);
-            indentationLevel--;
+        for(int i = 0; i < indentationLevel; i++)
+            m_writer.print(INDENT);
+        m_writer.print("if ");
+        node.jjtGetChild(0).jjtAccept(this,data);
+        printBlock(node, 1, data);
+        if(node.jjtGetNumChildren() == 3) {
+            m_writer.print("else");
+            printBlock(node, 2, data);
         }
-
         return null;
     }
 
     @Override
-    public Object visit(ASTBoolValue node, Object data) {
-        m_writer.print(capitalize(node.getValue()));
+    public Object visit(ASTWhileStmt node, Object data) {
+        for(int i = 0; i < indentationLevel; i++)
+            m_writer.print(INDENT);
+        m_writer.print("while ");
+        node.jjtGetChild(0).jjtAccept(this,data);
+        printBlock(node, 1, data);
         return null;
     }
 
+    /////////////////////////////////////////////////////////////////////////////
+    // 4.7 Point bonus: Operateur Coeur
     @Override
-    public Object visit(ASTIntValue node, Object data) {
-        m_writer.print(node.getValue());
-        return null;
-    }
+    public Object visit(ASTCoeurStmt node, Object data) {
+        if(indentationLevel >= 1) {
+            for(int i = 0; i < indentationLevel; i++)
+                m_writer.println(INDENT + "print(\"Compilateurs est le meilleur cours au monde\")");
+        } else
+            m_writer.println("print(\"Compilateurs est le meilleur cours au monde\")");
 
-    @Override
-    public Object visit(ASTRealValue node, Object data) {
-        m_writer.print(node.getValue());
         return null;
     }
 
